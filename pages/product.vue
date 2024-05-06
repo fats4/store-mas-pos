c468daa30f7d2a72f7f17edf0e1b4bd292fb0ce6
fats4 <muhammadfatu.2@gmail.com>
2024-05-06 14:36:11 +0700

    style

--- pages/product.vue
+++ pages/product.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
+    <div class="container mx-auto py-8 px-6">
         <h1 class="text-2xl font-bold mb-4">Products</h1>
-
+        <div class="flex flex-row flex-wrap gap-4 items-center justify-center">
+            <!-- Loop through each product in the array -->
+            <div v-for="(product) in auth.list" :key="product.id" class="bg-white shadow-md border border-blue-500 rounded-lg p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
+                <img :src="product.picture_url" alt="Product Image" class="w-full h-40 object-cover mb-4">
+                <h2 class="text-lg font-bold mb-2">{{ product.name }}</h2>
+                <p class="text-gray-500 mb-2">Name: {{ product.name }}</p>
+                <p class="text-gray-500 mb-2">Price: Rp {{ product.price }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
--- stores/store.js
+++ stores/store.js
@@ -7,63 +7,67 @@ export const useAuthStore = defineStore({
         password: '',
         isLoggedIn: false,
         token: '',
+        list: [],
     }),
     actions: {
         async login() {
             const router = useRouter();
 
             try {
-            const response = await fetch('https://mas-pos.appmedia.id/api/v1/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                email: this.email,
-                password: this.password,
-                }),
-            });
+                const response = await fetch('https://mas-pos.appmedia.id/api/v1/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        email: this.email,
+                        password: this.password,
+                    }),
+                });
 
-            if (response.ok) {
-                const data = await response.json();
-                sessionStorage.setItem('loginData', JSON.stringify(data));
-                console.log('Login data:', data);
+                if (response.ok) {
+                    const data = await response.json();
+                    sessionStorage.setItem('loginData', JSON.stringify(data));
+                    console.log('Login data:', data);
 
-                this.isLoggedIn = true;
-                this.token = data.data.token;
-                router.push('/product');
+                    this.isLoggedIn = true;
+                    this.token = data.data.token;
+                    router.push('/product');
 
-            } else {
-                console.error('Login failed');
-                alert('Login failed');
-            }
+                } else {
+                    console.error('Login failed');
+                    alert('Login failed');
+                }
             } catch (error) {
-            console.error('An error occurred:', error);
+                console.error('An error occurred:', error);
             }
         },
-        
+
         async fetchProducts() {
             try {
-            console.log('Token:', this.token);
-
-            const response = await fetch('https://mas-pos.appmedia.id/api/v1/product', {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${this.token}`,
-                },
-            });
-    
-            if (response.ok) {
-                const products = await response.json();
-                console.log('Products:', products);
-
-            } else {
-                console.error('Failed to fetch products token tidak didapat');
                 console.log('Token:', this.token);
-            }
-            } catch (error) {
 
-            console.error('An error occurred while fetching products:', error);
+                const response = await fetch('https://mas-pos.appmedia.id/api/v1/product', {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${this.token}`,
+                    },
+                });
+
+                if (response.ok) {
+                    const products = await response.json();
+                    console.log(products)
+
+                    this.list = products.data; // Store the fetched products in the state
+                    console.log(this.list);
+
+                } else {
+                    console.error('Failed to fetch products');
+                    console.log('Token:', this.token);
+                    alert('Tolong login');
+                }
+            } catch (error) {
+                console.error('An error occurred while fetching products:', error);
             }
         },
     },
